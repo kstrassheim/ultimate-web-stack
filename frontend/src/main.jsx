1b0c730e790acad2b46e5d64634a0ed7cccaa925
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2025-03-30 20:44:29 +0200

    added instance into msal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from './components/entraAuth';
-
 import { PublicClientApplication } from '@azure/msal-browser';
-
-// mock out instance if available
-export const msalInstance = new PublicClientApplication(msalConfig());
-
 
 import App from './App';
 import './index.css';
@@ -16,7 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MsalProvider instance={msalInstance}>
+      <MsalProvider instance={new PublicClientApplication(msalConfig())}>
         <App />
       </MsalProvider>
     </BrowserRouter>
